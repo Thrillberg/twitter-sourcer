ca04098a5d05a76a69b03b70ed14e11d71e41368
Eric Tillberg <eric.tillberg@gmail.com>
2019-06-01 20:35:05 -0400

    Add basic functionality

---
+++ README.md
@@ -0,0 +1,5 @@
+# Twitter Sourcer
+
+This browser extension is intended to display small, unobtrusive alerts to the
+user whenever they view a tweet published by a source either directly from or
+closely affiliated with a government.
---
+++ twitterSourcer.js
@@ -0,0 +1,60 @@
+let russianAccounts = [
+  "GovernmentRF",
+  "Pravitelstvo_RF",
+  "Russia",
+  "mfa_russia",
+  "KremlinRussia_E",
+  "KremlinRussia",
+  "MedvedevRussiaE",
+  "MedvedevRussia"
+];
+
+let stream = document.getElementById("stream-items-id");
+let config = { attributes: true, childList: true, subtree: false };
+
+let insertNodeAfter = (tweet) => {
+  let node = document.createElement("span");
+  node.classList.add("alert");
+  node.textContent = "🇷🇺  This is an official Russian account.";
+  stream.insertBefore(node, tweet.nextSibling);
+}
+
+let needsAlert = (child) => {
+  if (child.classList && child.classList.contains("tweet")) {
+    return russianAccounts.includes(child.getAttribute("data-screen-name"));
+  };
+
+  return false;
+}
+
+let insertAlerts = (tweet) => {
+  tweet.childNodes.forEach((child) => {
+    if (needsAlert(child)) {
+      insertNodeAfter(tweet);
+    };
+  });
+};
+
+let handleOriginalTweets = () => {
+  let tweets = stream.getElementsByClassName("stream-item");
+  for (let tweet of tweets) {
+    insertAlerts(tweet);
+  };
+};
+
+let handleSubsequentTweets = (mutationsList, observer) => {
+  for (let mutation of mutationsList) {
+    if (mutation.addedNodes.length > 0) {
+      mutation.addedNodes.forEach((tweet) => {
+        insertAlerts(tweet);
+      });
+
+      observer.disconnect();
+      new MutationObserver(handleSubsequentTweets).observe(stream, config);
+    };
+  };
+};
+
+handleOriginalTweets();
+let observer = new MutationObserver(handleSubsequentTweets);
+observer.observe(stream, config);
